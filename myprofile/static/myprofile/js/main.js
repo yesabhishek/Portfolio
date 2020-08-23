5584ee154547411a0855a7f0fdd7dc84a973c5d8
Abhishek Choudhury <choudhuryabhishek76@gmail.com>
2020-08-23 20:37:10 +0530

    Added the definitions

--- myprofile/static/myprofile/js/main.js
+++ myprofile/static/myprofile/js/main.js
@@ -5,12 +5,36 @@
   // Preloader
   $(window).on('load', function() {
     if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function() {
+      $('#preloader').delay(10).fadeOut('slow', function() {
         $(this).remove();
       });
     }
   });
 
+
+
+  // Navigation active state on scroll
+  var nav_sections = $('section');
+  var main_nav = $('.navbar, #mobile-nav');
+
+  $(window).on('scroll', function() {
+    var cur_pos = $(this).scrollTop() + 200;
+
+    nav_sections.each(function() {
+      var top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find('li').removeClass('active');
+        }
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+      }
+      if (cur_pos < 300) {
+        $(".nav-menu ul:first li:first").addClass('active');
+      }
+    });
+  });
   
 
 
